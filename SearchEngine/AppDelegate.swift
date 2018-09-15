e1b29cec486b7414ac8822b373fd1e52aa05d170
Oscar Gotting <gottingoscar@gmail.com>
2018-09-14 18:37:17 -0700

    Initial Commit

---
+++ SearchEngine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SearchEngine
+//
+//  Created by Oscar Götting on 9/14/18.
+//  Copyright © 2018 Oscar Götting. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SearchEngine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SearchEngine
+//
+//  Created by Oscar Götting on 9/14/18.
+//  Copyright © 2018 Oscar Götting. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
